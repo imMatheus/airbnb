0e7d978edc93c2011a99ab1d71d52d1d0c166f51
imMatheus <matheusmendesbarata03@gmail.com>
2022-12-20 22:03:26 +0100

    added correct scripts

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private prisma: PrismaService) {}
+
+  async getHello() {
+    // const users = await this.prisma;
     return 'Hello World!';
   }
 }
