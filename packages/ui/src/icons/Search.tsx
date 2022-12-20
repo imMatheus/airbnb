f1762a2b25db43febf4a8de601b29b5c622b9a19
imMatheus <matheusmendesbarata03@gmail.com>
2022-12-20 21:34:58 +0100

    added icons

--- packages/ui/src/icons/Search.tsx
+++ packages/ui/src/icons/Search.tsx
@@ -1,5 +1,27 @@
 import React from 'react'
 
 export const Search: React.FC = ({}) => {
-    return <div>search</div>
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 32 32'
+            role='img'
+            aria-hidden='true'
+            aria-label='Navigera till Twitter'
+            focusable='false'
+            style={{
+                overflow: 'visible',
+                height: '18px',
+                width: '18px',
+                display: 'block',
+                fill: 'none',
+                stroke: 'currentColor',
+                strokeWidth: '5.33333',
+            }}
+        >
+            <g fill='none'>
+                <path d='m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9' />
+            </g>
+        </svg>
+    )
 }
