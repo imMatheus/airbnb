7cba20f0a2e2ac01b20c5c7a67d0a299771eaeaf
imMatheus <matheusmendesbarata03@gmail.com>
2022-12-20 20:34:29 +0100

    added IG,twitter, fb icons

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,14 @@
-import { Button } from "ui";
+import { Button, Search, Facebook, Instagram, Twitter } from 'ui'
 
 export default function Web() {
-  return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Web</h1>
+            <Search />
+            <Button />
+            <Facebook />
+            <Instagram />
+            <Twitter />
+        </div>
+    )
 }
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+export * from './Button'
+export * from './icons/Facebook'
+export * from './icons/Instagram'
+export * from './icons/Search'
+export * from './icons/Twitter'
